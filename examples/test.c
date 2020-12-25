771f0c7a7c476ed157bd2e8b4927293432dca609
黄舒炜 <hsw18@mails.tsinghua.edu.cn>
2020-12-26 03:18:13 +0800

    modify tesc.c

---
+++ examples/test.c
@@ -0,0 +1,19 @@
+int a;
+
+int dothis(int a){
+    a = a + 1;
+}
+
+int main(){
+    a=0;
+    int a=1;
+    if (a==0){
+        for (int a=0;a<1;a=a+1){
+            int a=1;
+        }
+    }
+
+    if (a==0){
+        int x = a;
+    }
+}
---
+++ out.py
@@ -0,0 +1,22 @@
+from utils import *
+
+a = None
+def dothis(a_1):
+	global a
+	a_1 = (a_1 + 1)
+
+def main():
+	global a
+	a = 0
+	a_2 = 1
+	if (a_2 == 0):
+		a_4 = 0
+		while (a_4 < 1):
+			a_5 = 1
+			a_4 = (a_4 + 1)
+	if (a_2 == 0):
+		x = a_2
+
+if __name__ == '__main__':
+	main()
+
